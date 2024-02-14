a5946bcafa4190c202d5516620f74cbeb923f43f
Bonfigt <700231897@fvtc.edu>
2024-02-14 13:38:17 -0600

    added time

--- site.js
+++ site.js
@@ -1,20 +1,10 @@
 const hours = new Date().getHours() // get the current hour
-
-
 const isMorning = hours >= 4 && hours < 12 // is it morning?
-
-
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
 
-const showTime = (message, {hours, isMorning, isAfternoon, isEvening}) => {
-    message.querySelector('#welcome').textContent = hours
-    message.querySelector('#welcome').textContent = isMorning
-    message.querySelector('#welcome').textContent = isAfternoon
-    message.querySelector('#welcome').textContent = isEvening
-}
+const message = document.querySelector('#hi')
 
-const message = document.querySelector('#welcome')
-
-const div = message.querySelector('div')
-div.textContent = showTime
+if (isAfternoon) message.textContent = "good afternoon"
+if (isMorning) message.textContent = "good morning"
+if (isEvening) message.textContent = "good evening"
