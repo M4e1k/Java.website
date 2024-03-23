5c03342d46b961789f0de10d9b5d04cf007a7541
M4e1k <700231897@fvtc.edu>
2024-03-23 00:04:36 -0500

    added messaged

--- site.js
+++ site.js
@@ -1,10 +1,24 @@
 const hours = new Date().getHours() // get the current hour
+
+
 const isMorning = hours >= 4 && hours < 12 // is it morning?
+
+
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
 
-const message = document.querySelector('#hi')
+const showTime = (message, {hours, isMorning, isAfternoon, isEvening}) => {
+    message.querySelector('#welcome').textContent = hours
+    message.querySelector('#welcome').textContent = isMorning
+    message.querySelector('#welcome').textContent = isAfternoon
+    message.querySelector('#welcome').textContent = isEvening
+}
+document,addEventListener("DOMContentLoaded", function(){
+    const secretMessage = "Hello To you and people"
+    localStorage.setItem("It's a secret to everybody.", secretMessage)
+    console.log("added message")
+})
+const message = showTime.querySelector('#welcome')
 
-if (isAfternoon) message.textContent = "good afternoon"
-if (isMorning) message.textContent = "good morning"
-if (isEvening) message.textContent = "good evening"
+const div = message.querySelector('div')
+div.textContent = (showTime)
